c6e6e1b03f58edc05c085936c2cd1695110a3506
NghiemAnhTuan13 <ntuantn13022002@gmail.com>
2022-08-15 11:07:45 +0700

    check

--- nettruyen/main.js
+++ nettruyen/main.js
@@ -6,44 +6,43 @@ var reviewdiv = document.querySelector('.body1')
 function make_slide(amountSlideAppear){
     var widthitemanhmargin = reviewdiv.offsetWidth / amountSlideAppear;
     let widthallbox = widthitemanhmargin * listbox.length;
-    console.log(widthallbox)
     let count = 0
     let spacing = widthallbox - amountSlideAppear*widthitemanhmargin
-    wrapperbox.style.width = `1035.6px`
+    wrapperbox.style.width = `${widthallbox-widthitemanhmargin*2}px`
     function auto(){
         count += widthitemanhmargin
-        if (count > spacing){
+        if (count > spacing+1){
             count = 0;
         }
         if (count < widthitemanhmargin*2){
-            wrapperbox.style.width = `1035.6px`
+            wrapperbox.style.width = `${widthallbox-widthitemanhmargin*2}px`
             listbox[7].style.display='none'
             listbox[6].style.display='none'
             wrapperbox.style.transform = `translateX(${-count}px)`
         }
         else{
             wrapperbox.style.transform = `translateX(${-count}px)`
-            wrapperbox.style.width = `1380.8px`
+            wrapperbox.style.width = `${widthallbox}px`
             listbox[7].style.display='block'
             listbox[6].style.display='block'
 
         }
     setTimeout(auto,5000)
     }
-    setTimeout(auto,5000)
+    //setTimeout(auto,5000)
     btnright.addEventListener('click',function(){
         count += widthitemanhmargin
         if (count > spacing){
             count = 0;
         }
         if (count < widthitemanhmargin*2){
-            wrapperbox.style.width = `1035.6px`
+            wrapperbox.style.width = `${widthallbox-widthitemanhmargin*2}px`
             listbox[7].style.display='none'
             listbox[6].style.display='none'
             wrapperbox.style.transform = `translateX(${-count}px)`
         }
         else{
-            wrapperbox.style.width = `1380.8px`
+            wrapperbox.style.width = `${widthallbox}px`
             wrapperbox.style.transform = `translateX(${-count}px)`
             listbox[7].style.display='block'
             listbox[6].style.display='block'
@@ -56,11 +55,11 @@ function make_slide(amountSlideAppear){
             count = spacing;
         }
         if ( count <= widthitemanhmargin*2){
-            wrapperbox.style.width = `1035.6px`
+            wrapperbox.style.width = `${widthallbox-widthitemanhmargin*2}px`
             wrapperbox.style.transform = `translateX(${-count}px)`
             listbox[7].style.display='none'
-            if(count==345.19999999999993){
-                wrapperbox.style.width = `1208.2px`
+            if(count.toFixed(1)== widthitemanhmargin*2){
+                wrapperbox.style.width = `${widthallbox-widthitemanhmargin}px`
                 listbox[6].style.display='block'
             }
             else{
@@ -68,7 +67,7 @@ function make_slide(amountSlideAppear){
             }
         }
         else{
-            wrapperbox.style.width = `1380.8px`
+            wrapperbox.style.width = `${widthallbox}px`
             listbox[7].style.display='block'
             listbox[6].style.display='block'
             wrapperbox.style.transform = `translateX(${-count}px)`
@@ -106,7 +105,6 @@ top0[2].addEventListener('click',function(){
 })
 var a = document.querySelector('.heading2')
 var sticky = a.offsetTop
-console.log(window.pageYOffset)
 function mysticky(){
     if ( window.pageYOffset >= sticky){
         a.classList.add('sticky')
@@ -116,3 +114,28 @@ function mysticky(){
     }
 }
 window.onscroll = function() {mysticky()}
+
+var boxleft = document.querySelector('.boxleft')
+var boxright = document.querySelector('.boxright')
+var body = document.querySelector('.body0')
+var content = document.querySelectorAll('.content')
+var menu = document.querySelector('.menu')
+var bodymenu = document.querySelector('.bodymenu')
+var user = document.querySelector('.user')
+var last = document.querySelector('.last')
+document.addEventListener('DOMContentLoaded',function(){
+    window.addEventListener('resize',function(){
+        if(window.innerWidth >= 1254){
+            boxleft.style.width = `${(window.innerWidth-854)/2-5}px`
+            boxright.style.width = `${(window.innerWidth-854)/2-5}px`
+            reviewdiv.style.margin = `0 ${(window.innerWidth-854)/2-5}px 0 ${(window.innerWidth-854)/2-5}px`
+            content[1].style.margin = `0 ${(window.innerWidth-854)/2-5}px 0 ${(window.innerWidth-854)/2-5}px`
+            content[0].style.margin = `0 ${(window.innerWidth-854)/2-5}px 0 ${(window.innerWidth-854)/2-5}px`
+            menu.style.margin = `0 ${(window.innerWidth-854)/2-5}px 0 ${(window.innerWidth-854)/2-5}px`
+            btnright.style.right = `${(window.innerWidth-854)/2-5}px`
+            bodymenu.style.right = `${(window.innerWidth-854)/2}px`
+            user.style.right = `${(window.innerWidth-854)/2}px`
+            last.style.left = `${(window.innerWidth-854)/2+50}px`
+        }
+    })
+})
